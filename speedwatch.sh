54c1b4fcf3619e16701c40abd6652ab6a01d845f
Johannes Ballmann <fibsifan@users.noreply.github.com>
2015-03-04 00:13:30 +0100

    simplifying the script

--- speedwatch.sh
+++ speedwatch.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-if [ $UID -eq 0 ]; then
-	CONFIG_PATH=/etc/
-else
-	CONFIG_PATH=$HOME/.config/
-fi
+# Edit this to your needs:
+MYSQL_USER=speedwatch
+MYSQL_PASS=speedwatch
+MYSQL_DB=speedwatch
 
 SPEEDTEST_CLI=$(which speedtest-cli)
 if [ ! -x $SPEEDTEST_CLI ]; then
@@ -18,28 +17,28 @@ if [ ! -x $MYSQL_CLI ]; then
 	exit 1
 fi
 
-# check if config file exists
-if [ -f $CONFIG_PATH/speedwatch2.sh ]; then
-	. $CONFIG_PATH/speedwatch2.sh
-	$MYSQL_CLI --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB < speedwatch.sql
-else
-	echo 'Config file was missing, creating one in $CONFIG_PATH/speedwatch2.sh'
-	echo "Edit it to your needs and run this again."
-	echo "MYSQL_USER=speedwatch" >> $CONFIG_PATH/speedwatch2.sh
-	echo "MYSQL_PASS=speedwatch" >> $CONFIG_PATH/speedwatch2.sh
-	echo "MYSQL_DB=speedwatch" >> $CONFIG_PATH/speedwatch2.sh
-	exit 1
-fi
-
-$SPEEDWATCH_CLI --simple > /tmp/speedwatch2.tmp
-PING=grep Ping: /tmp/speedwatch2.tmp | sed s/'^Ping: \(.*\) ms$'/'\1'/
-DOWNLOAD=grep Download: /tmp/speedwatch2.tmp | sed s/'^Download: \(.*\) Mbits\/s$'/'\1'/
-UPLOAD=grep Upload: /tmp/speedwatch2.tmp | sed s/'^Upload: \(.*\) Mbits\/s$'/'\1'/
+# The Speed test:
+$SPEEDTEST_CLI --simple > /tmp/speedwatch2.tmp
+PING=$(grep Ping /tmp/speedwatch2.tmp | sed s/'^Ping: \(.*\) ms$'/'\1'/)
+DOWNLOAD=$(grep Download /tmp/speedwatch2.tmp | sed s/'^Download: \(.*\) Mbits\/s$'/'\1'/)
+UPLOAD=$(grep Upload /tmp/speedwatch2.tmp | sed s/'^Upload: \(.*\) Mbits\/s$'/'\1'/)
 rm /tmp/speedwatch2.tmp
 
 # Build SQL file
-echo "INSERT INTO SPEEDWATCH2 (DOWNLOAD, UPLOAD, PING)" > /tmp/speedwatch2.sql
-echo "VALUES ($DOWNLOAD, $UPLOAD, $PING);"
+cat << "EOF" > /tmp/speedwatch2.sql
+CREATE TABLE IF NOT EXISTS `SPEEDWATCH2` (
+	`ID` int unsigned NOT NULL auto_increment,
+	`TIMESTAMP` timestamp NOT NULL,
+	`DOWNLOAD` decimal(6,2) default 0,
+	`UPLOAD` decimal(6,2) default 0,
+	`PING` decimal(8,3) unsigned default 0,
+	PRIMARY KEY (`ID`)
+	);
+EOF
+
+echo "INSERT INTO SPEEDWATCH2 (DOWNLOAD, UPLOAD, PING)" >> /tmp/speedwatch2.sql
+echo "VALUES ($DOWNLOAD, $UPLOAD, $PING);" >> /tmp/speedwatch2.sql
 # Insert into mysql
-$MYSQL_CLI --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQ_DB < /tmp/speedwatch2.sql
-rm /tmp/speedwatch2.sql
+$MYSQL_CLI --user=$MYSQL_USER --password=$MYSQL_PASS $MYSQL_DB < /tmp/speedwatch2.sql
+#rm /tmp/speedwatch2.sql
+
--- speedwatch.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `SPEEDWATCH2` (
-	`ID` int unsigned NOT NULL auto_increment,
-	`TIMESTAMP` timestamp NOT NULL,
-	`DOWNLOAD` decimal(6,2) default 0,
-	`UPLOAD` decimal(6,2) default 0,
-	`PING` decimal(8,3) unsigned default 0,
-	PRIMARY KEY (`ID`)
-	)
